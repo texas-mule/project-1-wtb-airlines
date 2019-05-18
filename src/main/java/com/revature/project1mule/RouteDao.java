bacde5124de8cc9c25c9cf6a2c9fc0406011e1c1
joshparkerj <superfakek@gmail.com>
2019-05-17 19:24:29 -0500

    simplify connection logic in RouteDAO

--- src/main/java/com/revature/project1mule/RouteDao.java
+++ src/main/java/com/revature/project1mule/RouteDao.java
@@ -1,8 +1,6 @@
 package com.revature.project1mule;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,10 +13,7 @@ import com.revature.project1mule.flightschool.responsePojos.Route;
 public class RouteDao {
 
 	private static RouteDao uniqueInstance;
-	private static String url;
-	private static String username;
-	private static String password;
-	private static Connection conn;
+	private Connection conn;
 
 	public static RouteDao getDao() {
 		if (uniqueInstance == null)
@@ -30,30 +25,12 @@ public class RouteDao {
 		Properties props = new Properties();
 		try {
 			props.load(new FileInputStream("src/main/resources/config.properties"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		url = props.getProperty("elephant.url");
-		username = props.getProperty("elephant.username");
-		password = props.getProperty("elephant.password");
-	}
-
-	public static Connection getConnection() {
-		try {
 			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e2) {
-			e2.printStackTrace();
+			conn = DriverManager.getConnection(props.getProperty("elephant.url"),
+					props.getProperty("elephant.username"), props.getProperty("elephant.password"));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		if (conn == null) {
-			try {
-				conn = DriverManager.getConnection(url, username, password);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return conn;
 	}
 
 	public boolean insert(int rr, Object ai, Object aul, Object ar, Integer pi, Object as, int pa, String pn, Object an,
@@ -62,7 +39,6 @@ public class RouteDao {
 				+ "aircraft_range, pilot_id, aircraft_speed, pilot_age, pilot_name, "
 				+ "aircraft_name, aircraft_passenger_capacity, requested_passengers, name) "
 				+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-		Connection conn = getConnection();
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, rr);
 			pstmt.setInt(2, (Integer) ai);
@@ -89,7 +65,6 @@ public class RouteDao {
 		String sql = "UPDATE route SET requested_range = ?, aircraft_id = ?, aircraft_useful_load = ?, "
 				+ "aircraft_range = ?, pilot_id = ?, aircraft_speed = ?, pilot_age = ?, pilot_name = ?, "
 				+ "aircraft_name = ?, aircraft_passenger_capacity = ?, requested_passengers = ? " + "WHERE id = ?";
-		Connection conn = getConnection();
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, rr);
 			pstmt.setInt(2, (Integer) ai);
@@ -112,7 +87,6 @@ public class RouteDao {
 
 	public boolean pilotAssigned(int pi) {
 		String sql = "SELECT pilot_id FROM route WHERE pilot_id = ?";
-		Connection conn = getConnection();
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, pi);
 			ResultSet rs = pstmt.executeQuery();
